93a3d4ca9df0007697fe1d0fa5ca8721e45c6ade
Valentina Pucer <valentina.pucer@gmail.com>
2015-12-16 23:04:58 +0100

    Popravila sem tabelo1, sedaj so podatki nared za izdelavo grafov.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,15 +1,10 @@
 # 2. faza: Uvoz podatkov
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-#uvozi.druzine <- function() {
-  #return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      #row.names = 1,
-                      #col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      #fileEncoding = "Windows-1250"))
-#}
 require(dplyr)
 require(rvest)
 require(gsubfn)
+library(ggplot2)
+library(dplyr)
 
 #uvoz1
 uvozi.nesrece<-function(){
@@ -17,8 +12,7 @@ uvozi.nesrece<-function(){
                     fileEncoding = "UTF-8", header = FALSE,
                     na.strings = c("Unknown"), as.is = TRUE,
                    col.names=c("Leto","Tip.naravne.nesreče","Smrtne.žrtve","Škoda","Dogodek","Lokacija","V7")
-                   
-                  ))
+                   ))
 }
 
 cat("Uvažam podatke o naravnih nesrečah v ZDA...\n")
@@ -40,6 +34,7 @@ nesrece$Smrtne.žrtve <- gsub("\\?", "-", nesrece$Smrtne.žrtve)
 #filtracija
 attach(nesrece)
 oznaka<-c("Večja","Manjša")
+
 nesrece$Škoda <- gsub("\\$", "", nesrece$Škoda)
 nesrece$Škoda <- gsub(",", "", nesrece$Škoda)
 nesrece$Škoda <- gsub("^Billions$", "1000000000", nesrece$Škoda)
@@ -47,13 +42,15 @@ nesrece$Škoda <- gsub("million", "000000", nesrece$Škoda, ignore.case = TRUE)
 nesrece$Škoda <- gsub("billion", "000000000", nesrece$Škoda, ignore.case = TRUE)
 nesrece$Škoda<-gsub(" ", "", nesrece$Škoda)
 nesrece$Škoda<-as.numeric(nesrece$Škoda)
+nesrece$Smrtne.žrtve <- gsub(",", "", nesrece$Smrtne.žrtve)
+
 povprecje<-mean(nesrece$Škoda, na.rm = TRUE )
 stopnja<-character(nrow(nesrece))
 stopnja[nesrece$Škoda<povprecje]<-"Manjša"
 stopnja[nesrece$Škoda>povprecje]<-"Večja"
 Stopnja_skode<-factor(stopnja,levels=oznaka,ordered=TRUE)
 detach(nesrece)
-dodatenstolpec<-data.frame(Stopnja_skode)
+
 nesrece<-data.frame(nesrece,Stopnja_skode)
 
 
@@ -68,24 +65,22 @@ tabela1["Location"]<-NULL
 tabela1["Comments"]<-NULL
 tabela1$Fatalities <- gsub(",", "", tabela1$Fatalities)
 tabela1$Fatalities<-gsub("\\+", "", tabela1$Fatalities)
-#tabela1<-tabela1[c("Leto","št.smrtnih_žrtev","Stopnja_smrti")]
-
 tabela1$Fatalities<-gsub("\\(estimated)", "", tabela1$Fatalities)
 tabela1$Fatalities<-gsub("to"," ", tabela1$Fatalities)
 tabela1$Fatalities<-gsub("-"," ", tabela1$Fatalities)
+tabela1$Fatalities<-strapplyc(tabela1$Fatalities, "^([0-9]+)") %>% unlist() %>% as.numeric()
 
 attach(tabela1)
 oznaka1<-c("Veliko","Malo")
-stopnja1<-character(nrow(tabela))
-stopnja1[Fatalities<1000]<-"Malo"
+stopnja1<-character(nrow(tabela1))
+stopnja1[Fatalities<=1000]<-"Malo"
 stopnja1[Fatalities>1000]<-"Veliko"
-Stopnja_smrti<-factor(stopnja1,levels=oznaka1,ordered=TRUE)
+Stopnja_smrti<-factor(stopnja1,levels=oznaka1, ordered=TRUE)
 detach(tabela1)
 
-dodatenstolpec1<-data.frame(Stopnja_smrti)
+
 tabela1<-data.frame(tabela1,Stopnja_smrti)
 
 
-#iz prve tabele število smrtnih žrtev, poleg leta, oznaka(ogromno, malo).3stolpci
-#dva original, en dodan
+
 
