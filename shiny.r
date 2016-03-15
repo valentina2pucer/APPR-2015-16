d788f54fb21e8419a2d48edc89115fafc7cb7849
Valentina Pucer <valentina.pucer@gmail.com>
2016-03-15 18:57:37 +0100

    shiny tabela

--- shiny.r
+++ shiny.r
@@ -3,30 +3,26 @@ runApp("shiny")
 
 library(maps)
 library(mapproj)
+source("helpers.R")
 
-if ("server.R" %in% dir()){setwd("..")}
+#Aplikacija bo delovala tako, da boš lahko pogledal gostoto smrtnih žrtev na posameznih 
+#območjih
+#priprava ustrezne tabele:
+zem_tabela<-nesrece %>% select(Lokacija,Smrtne.žrtve)
 
-shinyServer(
-  function(input, output){ 
-  output$zem<-renderPlot({
-    data<-switch(input$var,
-          color <- switch(input$var, 
-            "Redko" = "darkgreen",
-            "Manj gosto" = "black",
-            "Gosto" = "darkorange",
-            "Najgosteje" = "darkviolet"))
-    
-    percent_map(var = data, 
-                color = color, 
-                legend.title = legend, 
-                max = input$range[2], 
-                min = input$range[1])
-    
-  })
-  }
-)
-shinyUI(fluidPage(
-  titlePanel("Gostota smrtnih žrtev"),
+attach(zem_tabela)
+oznaka1<-c("Redko","Manj gosto","Gosto","Najgosteje")
+stopnja1<-character(nrow(zem_tabela))
+stopnja1[Smrtne.žrtve<=100]<-"Redko"
+stopnja1[Smrtne.žrtve>100 & Smrtne.žrtve<300]<-"Manj gosto"
+stopnja1[Smrtne.žrtve>=300 & Smrtne.žrtve<1000]<-"Gosto"
+stopnja1[Smrtne.žrtve>=1000]<-"Najgosteje"
+Razvrstitev<-factor(stopnja1,levels=oznaka1, ordered=TRUE)
+detach(zem_tabela)
+
+zem_tabela<-data.frame(zem_tabela,Razvrstitev)
+
+shinyUI(fluidPage(titlePanel("Gostota smrtnih žrtev"),
   
   sidebarLayout(
     sidebarPanel(
@@ -37,10 +33,34 @@ shinyUI(fluidPage(
                   choices = c("Redko", "Manj gosto",
                               "Gosto", "Najgosteje"),
                   selected = "Redko")
-      
-     
       ),
     
     mainPanel(plotOutput("map"))
   )
 ))
+
+
+shinyServer(
+  function(input, output){ 
+  output$zem<-renderPlot({
+    data<-switch(input$var,
+                 "Redko" = zem_tabela$? kaj moram tukaj,
+                 "Manj gosto" = zem_tabela$Smrtne.žrtve,
+                 "Gosto" = zem_tabela$Smrtne.žrtve,
+                 "Najgosteje" =zem_tabela$Smrtne.žrtve ))
+  
+          color <- switch(input$var, 
+            "Redko" = "darkgreen",
+            "Manj gosto" = "black",
+            "Gosto" = "darkorange",
+            "Najgosteje" = "darkviolet"))
+    
+    percent_map(var = data,  #ali je tukaj kaka funkcijaza moj primer
+                color = color, 
+                legend.title = legend, 
+                max = input$range[2], 
+                min = input$range[1])
+    
+  })
+  }
+)
